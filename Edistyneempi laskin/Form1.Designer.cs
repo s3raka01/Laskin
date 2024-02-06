439e61b1924cfa3eaf916855a60004858c765ca1
Katariina Marjanna Rautio <s3raka01@students.osao.fi>
2024-02-06 09:56:16 +0200

    laskin

---
+++ Edistyneempi laskin/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Edistyneempi_laskin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nappi1 = new System.Windows.Forms.Button();
+            this.nappi2 = new System.Windows.Forms.Button();
+            this.nappi3 = new System.Windows.Forms.Button();
+            this.nappi4 = new System.Windows.Forms.Button();
+            this.nappi5 = new System.Windows.Forms.Button();
+            this.nappi6 = new System.Windows.Forms.Button();
+            this.nappi7 = new System.Windows.Forms.Button();
+            this.nappi8 = new System.Windows.Forms.Button();
+            this.nappi9 = new System.Windows.Forms.Button();
+            this.nappi0 = new System.Windows.Forms.Button();
+            this.jako = new System.Windows.Forms.Button();
+            this.kerto = new System.Windows.Forms.Button();
+            this.miinus = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.yhtakuin = new System.Windows.Forms.Button();
+            this.Cnappain = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // nappi1
+            // 
+            this.nappi1.Location = new System.Drawing.Point(21, 152);
+            this.nappi1.Name = "nappi1";
+            this.nappi1.Size = new System.Drawing.Size(28, 23);
+            this.nappi1.TabIndex = 0;
+            this.nappi1.Text = "1";
+            this.nappi1.UseVisualStyleBackColor = true;
+            this.nappi1.Click += new System.EventHandler(this.nappi1_Click);
+            // 
+            // nappi2
+            // 
+            this.nappi2.Location = new System.Drawing.Point(55, 152);
+            this.nappi2.Name = "nappi2";
+            this.nappi2.Size = new System.Drawing.Size(29, 23);
+            this.nappi2.TabIndex = 1;
+            this.nappi2.Text = "2";
+            this.nappi2.UseVisualStyleBackColor = true;
+            this.nappi2.Click += new System.EventHandler(this.nappi2_Click);
+            // 
+            // nappi3
+            // 
+            this.nappi3.Location = new System.Drawing.Point(90, 152);
+            this.nappi3.Name = "nappi3";
+            this.nappi3.Size = new System.Drawing.Size(29, 23);
+            this.nappi3.TabIndex = 2;
+            this.nappi3.Text = "3";
+            this.nappi3.UseVisualStyleBackColor = true;
+            this.nappi3.Click += new System.EventHandler(this.nappi3_Click);
+            // 
+            // nappi4
+            // 
+            this.nappi4.Location = new System.Drawing.Point(21, 123);
+            this.nappi4.Name = "nappi4";
+            this.nappi4.Size = new System.Drawing.Size(28, 23);
+            this.nappi4.TabIndex = 3;
+            this.nappi4.Text = "4";
+            this.nappi4.UseVisualStyleBackColor = true;
+            this.nappi4.Click += new System.EventHandler(this.nappi4_Click);
+            // 
+            // nappi5
+            // 
+            this.nappi5.Location = new System.Drawing.Point(55, 123);
+            this.nappi5.Name = "nappi5";
+            this.nappi5.Size = new System.Drawing.Size(29, 23);
+            this.nappi5.TabIndex = 4;
+            this.nappi5.Text = "5";
+            this.nappi5.UseVisualStyleBackColor = true;
+            this.nappi5.Click += new System.EventHandler(this.nappi5_Click);
+            // 
+            // nappi6
+            // 
+            this.nappi6.Location = new System.Drawing.Point(90, 123);
+            this.nappi6.Name = "nappi6";
+            this.nappi6.Size = new System.Drawing.Size(29, 23);
+            this.nappi6.TabIndex = 5;
+            this.nappi6.Text = "6";
+            this.nappi6.UseVisualStyleBackColor = true;
+            this.nappi6.Click += new System.EventHandler(this.nappi6_Click);
+            // 
+            // nappi7
+            // 
+            this.nappi7.Location = new System.Drawing.Point(21, 94);
+            this.nappi7.Name = "nappi7";
+            this.nappi7.Size = new System.Drawing.Size(28, 23);
+            this.nappi7.TabIndex = 6;
+            this.nappi7.Text = "7";
+            this.nappi7.UseVisualStyleBackColor = true;
+            this.nappi7.Click += new System.EventHandler(this.nappi7_Click);
+            // 
+            // nappi8
+            // 
+            this.nappi8.Location = new System.Drawing.Point(55, 94);
+            this.nappi8.Name = "nappi8";
+            this.nappi8.Size = new System.Drawing.Size(29, 23);
+            this.nappi8.TabIndex = 7;
+            this.nappi8.Text = "8";
+            this.nappi8.UseVisualStyleBackColor = true;
+            this.nappi8.Click += new System.EventHandler(this.nappi8_Click);
+            // 
+            // nappi9
+            // 
+            this.nappi9.Location = new System.Drawing.Point(90, 94);
+            this.nappi9.Name = "nappi9";
+            this.nappi9.Size = new System.Drawing.Size(29, 23);
+            this.nappi9.TabIndex = 8;
+            this.nappi9.Text = "9";
+            this.nappi9.UseVisualStyleBackColor = true;
+            this.nappi9.Click += new System.EventHandler(this.nappi9_Click);
+            // 
+            // nappi0
+            // 
+            this.nappi0.Location = new System.Drawing.Point(55, 181);
+            this.nappi0.Name = "nappi0";
+            this.nappi0.Size = new System.Drawing.Size(29, 23);
+            this.nappi0.TabIndex = 9;
+            this.nappi0.Text = "0";
+            this.nappi0.UseVisualStyleBackColor = true;
+            this.nappi0.Click += new System.EventHandler(this.nappi0_Click);
+            // 
+            // jako
+            // 
+            this.jako.Location = new System.Drawing.Point(124, 65);
+            this.jako.Name = "jako";
+            this.jako.Size = new System.Drawing.Size(30, 23);
+            this.jako.TabIndex = 10;
+            this.jako.Text = "/";
+            this.jako.UseVisualStyleBackColor = true;
+            this.jako.Click += new System.EventHandler(this.jako_Click);
+            // 
+            // kerto
+            // 
+            this.kerto.Location = new System.Drawing.Point(124, 94);
+            this.kerto.Name = "kerto";
+            this.kerto.Size = new System.Drawing.Size(30, 23);
+            this.kerto.TabIndex = 11;
+            this.kerto.Text = "*";
+            this.kerto.UseVisualStyleBackColor = true;
+            this.kerto.Click += new System.EventHandler(this.kerto_Click);
+            // 
+            // miinus
+            // 
+            this.miinus.Location = new System.Drawing.Point(125, 123);
+            this.miinus.Name = "miinus";
+            this.miinus.Size = new System.Drawing.Size(30, 23);
+            this.miinus.TabIndex = 12;
+            this.miinus.Text = "-";
+            this.miinus.UseVisualStyleBackColor = true;
+            this.miinus.Click += new System.EventHandler(this.miinus_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(125, 152);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(31, 23);
+            this.plus.TabIndex = 13;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // yhtakuin
+            // 
+            this.yhtakuin.Location = new System.Drawing.Point(126, 181);
+            this.yhtakuin.Name = "yhtakuin";
+            this.yhtakuin.Size = new System.Drawing.Size(31, 23);
+            this.yhtakuin.TabIndex = 14;
+            this.yhtakuin.Text = "=";
+            this.yhtakuin.UseVisualStyleBackColor = true;
+            this.yhtakuin.Click += new System.EventHandler(this.yhtakuin_Click);
+            // 
+            // Cnappain
+            // 
+            this.Cnappain.Location = new System.Drawing.Point(90, 65);
+            this.Cnappain.Name = "Cnappain";
+            this.Cnappain.Size = new System.Drawing.Size(28, 23);
+            this.Cnappain.TabIndex = 15;
+            this.Cnappain.Text = "C";
+            this.Cnappain.UseVisualStyleBackColor = true;
+            this.Cnappain.Click += new System.EventHandler(this.Cnappain_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(22, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(130, 20);
+            this.textBox3.TabIndex = 16;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(22, 40);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(130, 20);
+            this.textBox4.TabIndex = 17;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(169, 230);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Cnappain);
+            this.Controls.Add(this.yhtakuin);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.miinus);
+            this.Controls.Add(this.kerto);
+            this.Controls.Add(this.jako);
+            this.Controls.Add(this.nappi0);
+            this.Controls.Add(this.nappi9);
+            this.Controls.Add(this.nappi8);
+            this.Controls.Add(this.nappi7);
+            this.Controls.Add(this.nappi6);
+            this.Controls.Add(this.nappi5);
+            this.Controls.Add(this.nappi4);
+            this.Controls.Add(this.nappi3);
+            this.Controls.Add(this.nappi2);
+            this.Controls.Add(this.nappi1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button nappi1;
+        private System.Windows.Forms.Button nappi2;
+        private System.Windows.Forms.Button nappi3;
+        private System.Windows.Forms.Button nappi4;
+        private System.Windows.Forms.Button nappi5;
+        private System.Windows.Forms.Button nappi6;
+        private System.Windows.Forms.Button nappi7;
+        private System.Windows.Forms.Button nappi8;
+        private System.Windows.Forms.Button nappi9;
+        private System.Windows.Forms.Button nappi0;
+        private System.Windows.Forms.Button jako;
+        private System.Windows.Forms.Button kerto;
+        private System.Windows.Forms.Button miinus;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button yhtakuin;
+        private System.Windows.Forms.Button Cnappain;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Edistyneempi laskin/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Edistyneempi_laskin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int luku1 = 0;
+        int luku2 = 0;
+        String valinta;
+        int tulos;
+
+        private void nappi1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text += 1;
+        }
+
+        private void nappi2_Click(object sender, EventArgs e)
+        {
+            textBox3.Text += 2;
+        }
+
+        private void nappi4_Click(object sender, EventArgs e)
+        {
+            textBox3.Text += 4;
+        }
+
+        private void nappi3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text += 3;
+        }
+
+        private void nappi5_Click(object sender, EventArgs e)
+        {
+            textBox3.Text += 5;
+        }
+
+        private void nappi6_Click(object sender, EventArgs e)
+        {
+            textBox3.Text += 6;
+        }
+
+        private void nappi7_Click(object sender, EventArgs e)
+        {
+            textBox3.Text += 7;
+        }
+
+        private void nappi8_Click(object sender, EventArgs e)
+        {
+            textBox3.Text += 8;
+        }
+
+        private void nappi9_Click(object sender, EventArgs e)
+        {
+            textBox3.Text += 9;
+        }
+
+        private void nappi0_Click(object sender, EventArgs e)
+        {
+            textBox3.Text += 0;
+        }
+
+        private void kerto_Click(object sender, EventArgs e)
+        {
+                textBox3.Text += " * ";
+                valinta = "*";
+        }
+
+        private void jako_Click(object sender, EventArgs e)
+        {
+                textBox3.Text += " / ";
+                valinta = "/";
+        }
+
+        private void miinus_Click(object sender, EventArgs e)
+        {
+                textBox3.Text += " - ";
+                valinta = "-";
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+                textBox3.Text += " + "; 
+                valinta = "+";
+        }
+
+        private void yhtakuin_Click(object sender, EventArgs e)
+        {
+            string[] splitted = textBox3.Text.Split(' ');
+            if (splitted.Length >= 2 && splitted[2] != "")
+            {
+                int luku1 = int.Parse(splitted[0]);
+                int luku2 = int.Parse(splitted[2]);
+
+                double answer = 0;
+                bool error = false;
+                switch (valinta)
+                {
+                    case "+":
+                        answer = luku1 + luku2;
+                        break;
+                    case "-":
+                        answer = luku1 - luku2;
+                        break;
+                    case "*":
+                        answer = luku1 * luku2;
+                        break;
+                    case "/":
+                        if (luku2 != 0)
+                        { 
+                            answer = luku1 / luku2;
+                        }
+                        else
+                        {
+                            error = true;
+                        }
+                        break;
+                    default:
+                        error = true;
+                        break;
+                }
+                if(error)
+                {
+                    textBox4.Text = "ERROR";
+                }
+                else
+                {
+                    textBox4.Text = answer.ToString();
+                }
+
+                textBox3.Text += " = ";
+            }
+        }
+
+        private void Cnappain_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            textBox4.Clear();
+            tulos = (0);
+            luku1 = (0);
+            luku2 = (0);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
